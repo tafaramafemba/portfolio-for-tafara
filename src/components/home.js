d37222c82730ee9ea5aa9408d48ec6c0fd834bce
tafaramafemba <tafara.maf@gmail.com>
2022-06-11 14:23:08 +0200

    Home screen image, text and animations

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,15 @@
 import '../home.css'
 
-function Home(){
+function Home() {
 
- return (
-<h1>Hie</h1>
- ) 
+  return (
+    <div className='home'>
+      <img src='tafara-img.png' className='tafara_img'></img>
+      <div className='text'>
+      <span className='tafara-maf'>Hello, my name is Tafara Mafemba.</span>
+      </div>
+    </div>
+  )
 }
 
 export default Home;
